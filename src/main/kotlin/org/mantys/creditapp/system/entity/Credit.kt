b48e76fa7d8527c6c13a58ee2f7221b19420235f
odantonio <obdantonio@terra.com.br>
2024-02-25 18:48:22 -0300

    camada de persistencia finalizada

---
+++ src/main/kotlin/org/mantys/creditapp/system/entity/Credit.kt
@@ -0,0 +1,19 @@
+package org.mantys.creditapp.system.entity
+
+import jakarta.persistence.*
+import org.mantys.creditapp.system.enums.Status
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.*
+
+@Entity
+@Table(name = "credits")
+data class Credit (
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) var creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) var dayOfInstallment: LocalDate,
+    @Column(nullable = false) var numberOfInstallments: Int = 0,
+    @Enumerated var status: Status = Status.IN_PROGRESS,
+    @ManyToOne val customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id : Long? = null
+)
---
+++ src/main/kotlin/org/mantys/creditapp/system/enums/Status.kt
@@ -0,0 +1,7 @@
+package org.mantys.creditapp.system.enums
+
+enum class Status {
+    IN_PROGRESS,
+    APPROVED,
+    REJECTED
+}
