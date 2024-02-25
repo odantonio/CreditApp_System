5149cd4c1c86d19cddc1506e3bb755ad0a5aac7c
odantonio <obdantonio@terra.com.br>
2024-02-25 19:05:31 -0300

    iniciando projeto

---
+++ src/main/kotlin/org/mantys/creditapp/system/CreditappSystemApplication.kt
@@ -0,0 +1,11 @@
+package org.mantys.creditapp.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditappSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditappSystemApplication>(*args)
+}
